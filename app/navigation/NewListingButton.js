c1bb752c58a64750e3d11ff4433db9a70c81146b
Farah Al Fahim <cadetfhm@gmail.com>
2020-10-10 12:57:52 +0600

    Frontend with Navigation

---
+++ app/navigation/NewListingButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+
+function NewListingButton({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="plus-circle"
+          color={colors.white}
+          size={35}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderColor: colors.white,
+    borderRadius: 30,
+    borderWidth: 5,
+    bottom: 15,
+    height: 60,
+    justifyContent: "center",
+    width: 60,
+  },
+});
+
+export default NewListingButton;
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,24 +1,30 @@
 import React from "react";
-import { StyleSheet, Image } from "react-native";
+import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
   email: Yup.string().required().email().label("Email"),
   password: Yup.string().required().min(8).label("Password"),
 });
 
-function LoginScreen(props) {
+function RegisterScreen() {
   return (
     <Screen style={styles.container}>
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
       <AppForm
-        initialValues={{ email: "", password: " " }}
+        initialValues={{ name: "", email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <AppFormField
+          autoCorrect={false}
+          icon="account"
+          name="name"
+          placeholder="Name"
+        />
         <AppFormField
           autoCapitalize="none"
           autoCorrect={false}
@@ -26,7 +32,7 @@ function LoginScreen(props) {
           keyboardType="email-address"
           name="email"
           placeholder="Email"
-          textContentType="emailAddress" //only in IOS
+          textContentType="emailAddress"
         />
         <AppFormField
           autoCapitalize="none"
@@ -35,9 +41,9 @@ function LoginScreen(props) {
           name="password"
           placeholder="Password"
           secureTextEntry
-          textContentType="password" //only in IOS
+          textContentType="password"
         />
-        <SubmitButton title="Login" />
+        <SubmitButton title="Register" />
       </AppForm>
     </Screen>
   );
@@ -47,13 +53,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
   },
-  logo: {
-    width: 80,
-    height: 80,
-    alignSelf: "center",
-    marginTop: 50,
-    marginBottom: 20,
-  },
 });
 
-export default LoginScreen;
+export default RegisterScreen;
